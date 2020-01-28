9a7fab00beb8c95c2cc7160cb8305e9a75aeb201
Peter Griffin <peterhughgriffin@gmail.com>
2020-01-28 21:35:54 +0000

    Functionalise Live Data Feature

--- EnergyData.py
+++ EnergyData.py
@@ -18,50 +18,67 @@ def GetKey():
         Key = file.read()
     return Key
 
-Key=GetKey()
-
-if 'xml' in locals():
-    print('Fetching data')
-    with open('LastAccessed.txt', 'r') as file:
-        StoredTime = datetime.strptime(file.read(),'%Y-%m-%d %H:%M:%S')
-    NowTime = datetime.now()
-    Diff = NowTime-StoredTime
-    print('Data last accessed '+ str(Diff)[0:-7]+' ago')
-    FiveMin = timedelta(minutes=5)
-    if Diff > FiveMin:
-        print('Refreshing data')
-        with open('LastAccessed.txt', 'w') as file:
-            file.write(str(NowTime)[0:-7])
+def GetLiveData():
+    
+    Key=GetKey()
+    
+    if 'xml' in locals():
+        print('Fetching data')
+        with open('LastAccessed.txt', 'r') as file:
+            StoredTime = datetime.strptime(file.read(),'%Y-%m-%d %H:%M:%S')
+        NowTime = datetime.now()
+        Diff = NowTime-StoredTime
+        print('Data last accessed '+ str(Diff)[0:-7]+' ago')
+        FiveMin = timedelta(minutes=5)
+        if Diff > FiveMin:
+            print('Refreshing data')
+            with open('LastAccessed.txt', 'w') as file:
+                file.write(str(NowTime)[0:-7])
+            #Data can be accessed from the API at the Elexon portal: https://www.elexonportal.co.uk/scripting
+            url='https://downloads.elexonportal.co.uk/fuel/download/latest?key='+Key
+            xml = objectify.parse(urllib.request.urlopen(url))
+            root=xml.getroot()
+    else:
+        print('Fetching new data')
         #Data can be accessed from the API at the Elexon portal: https://www.elexonportal.co.uk/scripting
         url='https://downloads.elexonportal.co.uk/fuel/download/latest?key='+Key
         xml = objectify.parse(urllib.request.urlopen(url))
         root=xml.getroot()
-
-else:
-    print('Fetching new data')
-    #Data can be accessed from the API at the Elexon portal: https://www.elexonportal.co.uk/scripting
-    url='https://downloads.elexonportal.co.uk/fuel/download/latest?key='+Key
-    xml = objectify.parse(urllib.request.urlopen(url))
-    root=xml.getroot()
+    
+    #%%
+    #Initialise lists for extracting data to
+    Fuel =[]
+    Energy =[]
+    Pct =[]
+    
+    #Extract data
+    for child in root.INST:
+        for e in child.getchildren():
+            Fuel.append(e.attrib['TYPE'])
+            Energy.append(int(e.attrib['VAL']))
+            Pct.append(float(e.attrib['PCT']))
+    
+    # Place data into a Pandas Dataframe
+    Data = {'Energy': Energy, 'Pct': Pct}
+    df = pd.DataFrame(Data, index = Fuel)
+    
+    return df
 
 #%%
-#Initialise lists for extracting data from
-Fuel =[]
-Energy =[]
-Pct =[]
 
-for child in root.INST:
-    for e in child.getchildren():
-        Fuel.append(e.attrib['TYPE'])
-        Energy.append(int(e.attrib['VAL']))
-        Pct.append(float(e.attrib['PCT']))
 
-# Place data into a Pandas Dataframe
-Data = {'Energy': Energy, 'Pct': Pct}
-df = pd.DataFrame(Data, index = Fuel)
+LiveData = GetLiveData()
 
 # Sort the data Descending in terms of energy contribution
-df.sort_values(by='Energy', ascending=False, inplace=True)
+LiveData = LiveData.sort_values(by='Energy', ascending=False)
 
 
-df.plot(y='Energy',kind='pie', autopct='%1.1f%%', shadow=False, startangle=0)
+# Plot pie chart
+ax1 = LiveData.plot.pie(y='Energy', autopct='%1.1f%%', shadow=False, startangle=0, legend=False)
+ax1.set_title('Current Percentage Share of UK Electricity mix')
+ax1.set_ylabel('')
+
+
+
+
+
